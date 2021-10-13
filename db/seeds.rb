980eb9e2a59fa303b90e6823658d97358c7e69f0
Mike Miller <mikemille999@gmail.com>
2021-10-13 11:40:55 -0500

    adds Product model and adds data to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create({name: 'Desktop', price: 1750.0, image_url: '', description: 'A desktop meant to be able to process mosst things.'})
+Product.create({name: 'Laptop', price: 1350.0, image_url: '', description: 'At least its portable.'})
+Product.create({name: 'mechanical keyboard', price: 60.0, image_url: '', description: 'A loud keyboard that typically gives better feedback on when you activate the switch for a key press.'})
